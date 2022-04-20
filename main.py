45a8e290cacff9824dda4c5ca9df0da5c914594d
Quang <quangtd.24it@gmail.com>
2022-04-20 20:29:59 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,465 @@
+from gc import callbacks
+import math
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint
+from imutils import paths
+
+# Setting seed for reproducibiltiy
+SEED = 42
+keras.utils.set_random_seed(SEED)
+
+"""
+## Prepare the data
+"""
+
+NUM_CLASSES = 4
+INPUT_SHAPE = (640, 640, 3)
+
+# (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
+
+# print(f"x_train shape: {x_train.shape} - y_train shape: {y_train.shape}")
+# print(f"x_test shape: {x_test.shape} - y_test shape: {y_test.shape}")
+
+# DATA
+BUFFER_SIZE = 512
+BATCH_SIZE = 32
+
+# AUGMENTATION
+IMAGE_SIZE = 72
+PATCH_SIZE = 6
+NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
+
+# OPTIMIZER
+LEARNING_RATE = 0.001
+WEIGHT_DECAY = 0.0001
+
+# TRAINING
+EPOCHS = 50
+
+# ARCHITECTURE
+LAYER_NORM_EPS = 1e-6
+TRANSFORMER_LAYERS = 8
+PROJECTION_DIM = 64
+NUM_HEADS = 4
+TRANSFORMER_UNITS = [
+    PROJECTION_DIM * 2,
+    PROJECTION_DIM,
+]
+MLP_HEAD_UNITS = [2048, 1024]
+
+trainGen = ImageDataGenerator().flow_from_directory(
+    "train",
+    class_mode="sparse",
+    target_size=(640, 640),
+    color_mode="rgb",
+    shuffle=True,
+    batch_size=BATCH_SIZE,
+)
+
+validGen = ImageDataGenerator().flow_from_directory(
+    "validation",
+    class_mode="sparse",
+    target_size=(640, 640),
+    color_mode="rgb",
+    shuffle=False,
+    batch_size=BATCH_SIZE,
+)
+
+data_augmentation = keras.Sequential(
+    [
+        layers.Normalization(),
+        layers.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(factor=0.02),
+        layers.RandomZoom(height_factor=0.2, width_factor=0.2),
+    ],
+    name="data_augmentation",
+)
+# Compute the mean and the variance of the training data for normalization.
+# data_augmentation.layers[0].adapt(trainGen)
+
+class ShiftedPatchTokenization(layers.Layer):
+    def __init__(
+        self,
+        image_size=IMAGE_SIZE,
+        patch_size=PATCH_SIZE,
+        num_patches=NUM_PATCHES,
+        projection_dim=PROJECTION_DIM,
+        vanilla=False,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.vanilla = vanilla  # Flag to swtich to vanilla patch extractor
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.half_patch = patch_size // 2
+        self.flatten_patches = layers.Reshape((num_patches, -1))
+        self.projection_dim = projection_dim
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.layer_norm = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'image_size': self.image_size,
+            'patch_size': self.patch_size,
+            'num_patches': self.num_patches,
+            'projection_dim': self.projection_dim,
+            'vanilla': self.vanilla,
+        })
+        return config
+    def crop_shift_pad(self, images, mode):
+        # Build the diagonally shifted images
+        if mode == "left-up":
+            crop_height = self.half_patch
+            crop_width = self.half_patch
+            shift_height = 0
+            shift_width = 0
+        elif mode == "left-down":
+            crop_height = 0
+            crop_width = self.half_patch
+            shift_height = self.half_patch
+            shift_width = 0
+        elif mode == "right-up":
+            crop_height = self.half_patch
+            crop_width = 0
+            shift_height = 0
+            shift_width = self.half_patch
+        else:
+            crop_height = 0
+            crop_width = 0
+            shift_height = self.half_patch
+            shift_width = self.half_patch
+
+        # Crop the shifted images and pad them
+        crop = tf.image.crop_to_bounding_box(
+            images,
+            offset_height=crop_height,
+            offset_width=crop_width,
+            target_height=self.image_size - self.half_patch,
+            target_width=self.image_size - self.half_patch,
+        )
+        shift_pad = tf.image.pad_to_bounding_box(
+            crop,
+            offset_height=shift_height,
+            offset_width=shift_width,
+            target_height=self.image_size,
+            target_width=self.image_size,
+        )
+        return shift_pad
+
+    def call(self, images):
+        if not self.vanilla:
+            # Concat the shifted images with the original image
+            images = tf.concat(
+                [
+                    images,
+                    self.crop_shift_pad(images, mode="left-up"),
+                    self.crop_shift_pad(images, mode="left-down"),
+                    self.crop_shift_pad(images, mode="right-up"),
+                    self.crop_shift_pad(images, mode="right-down"),
+                ],
+                axis=-1,
+            )
+        # Patchify the images and flatten it
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        flat_patches = self.flatten_patches(patches)
+        if not self.vanilla:
+            # Layer normalize the flat patches and linearly project it
+            tokens = self.layer_norm(flat_patches)
+            tokens = self.projection(tokens)
+        else:
+            # Linearly project the flat patches
+            tokens = self.projection(flat_patches)
+        return (tokens, patches)
+
+
+"""
+### Visualize the patches
+"""
+
+# Get a random image from the training dataset
+# and resize the image
+
+# image = trainGen[np.random.choice(range(trainGen.shape[0]))]
+# resized_image = tf.image.resize(
+#     tf.convert_to_tensor([image]), size=(IMAGE_SIZE, IMAGE_SIZE)
+# )
+
+# # Vanilla patch maker: This takes an image and divides into
+# # patches as in the original ViT paper
+# (token, patch) = ShiftedPatchTokenization(vanilla=True)(resized_image / 255.0)
+# (token, patch) = (token[0], patch[0])
+# n = patch.shape[0]
+# count = 1
+# plt.figure(figsize=(4, 4))
+# for row in range(n):
+#     for col in range(n):
+#         plt.subplot(n, n, count)
+#         count = count + 1
+#         image = tf.reshape(patch[row][col], (PATCH_SIZE, PATCH_SIZE, 3))
+#         plt.imshow(image)
+#         plt.axis("off")
+# plt.show()
+
+# # Shifted Patch Tokenization: This layer takes the image, shifts it
+# # diagonally and then extracts patches from the concatinated images
+# (token, patch) = ShiftedPatchTokenization(vanilla=False)(resized_image / 255.0)
+# (token, patch) = (token[0], patch[0])
+# n = patch.shape[0]
+# shifted_images = ["ORIGINAL", "LEFT-UP", "LEFT-DOWN", "RIGHT-UP", "RIGHT-DOWN"]
+# for index, name in enumerate(shifted_images):
+#     print(name)
+#     count = 1
+#     plt.figure(figsize=(4, 4))
+#     for row in range(n):
+#         for col in range(n):
+#             plt.subplot(n, n, count)
+#             count = count + 1
+#             image = tf.reshape(patch[row][col], (PATCH_SIZE, PATCH_SIZE, 5 * 3))
+#             plt.imshow(image[..., 3 * index : 3 * index + 3])
+#             plt.axis("off")
+#     plt.show()
+
+class PatchEncoder(layers.Layer):
+    def __init__(
+        self, num_patches=NUM_PATCHES, projection_dim=PROJECTION_DIM, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.num_patches = num_patches
+        self.projection_dim = projection_dim
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+        self.positions = tf.range(start=0, limit=self.num_patches, delta=1)
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'num_patches': self.num_patches,
+            'projection_dim': self.projection_dim,
+        })
+        return config
+    def call(self, encoded_patches):
+        encoded_positions = self.position_embedding(self.positions)
+        encoded_patches = encoded_patches + encoded_positions
+        return encoded_patches
+
+
+class MultiHeadAttentionLSA(tf.keras.layers.MultiHeadAttention):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # The trainable temperature term. The initial value is
+        # the square root of the key dimension.
+        self.tau = tf.Variable(math.sqrt(float(self._key_dim)), trainable=True)
+
+    def _compute_attention(self, query, key, value, attention_mask=None, training=None):
+        query = tf.multiply(query, 1.0 / self.tau)
+        attention_scores = tf.einsum(self._dot_product_equation, key, query)
+        attention_scores = self._masked_softmax(attention_scores, attention_mask)
+        attention_scores_dropout = self._dropout_layer(
+            attention_scores, training=training
+        )
+        attention_output = tf.einsum(
+            self._combine_equation, attention_scores_dropout, value
+        )
+        return attention_output, attention_scores
+
+
+"""
+## Implement the MLP
+"""
+
+
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+
+
+# Build the diagonal attention mask
+diag_attn_mask = 1 - tf.eye(NUM_PATCHES)
+diag_attn_mask = tf.cast([diag_attn_mask], dtype=tf.int8)
+
+"""
+## Build the ViT
+"""
+
+
+def create_vit_classifier(vanilla=False):
+    inputs = layers.Input(shape=INPUT_SHAPE)
+    # Augment data.
+    augmented = data_augmentation(inputs)
+    # Create patches.
+    (tokens, _) = ShiftedPatchTokenization(vanilla=vanilla)(augmented)
+    # Encode patches.
+    encoded_patches = PatchEncoder()(tokens)
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(TRANSFORMER_LAYERS):
+        # Layer normalization 1.
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        if not vanilla:
+            attention_output = MultiHeadAttentionLSA(
+                num_heads=NUM_HEADS, key_dim=PROJECTION_DIM, dropout=0.1
+            )(x1, x1, attention_mask=diag_attn_mask)
+        else:
+            attention_output = layers.MultiHeadAttention(
+                num_heads=NUM_HEADS, key_dim=PROJECTION_DIM, dropout=0.1
+            )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=TRANSFORMER_UNITS, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x3, x2])
+
+    # Create a [batch_size, projection_dim] tensor.
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    representation = layers.Flatten()(representation)
+    representation = layers.Dropout(0.5)(representation)
+    # Add MLP.
+    features = mlp(representation, hidden_units=MLP_HEAD_UNITS, dropout_rate=0.5)
+    # Classify outputs.
+    logits = layers.Dense(NUM_CLASSES)(features)
+    # Create the Keras model.
+    model = keras.Model(inputs=inputs, outputs=logits)
+    return model
+
+
+"""
+## Compile, train, and evaluate the mode
+"""
+
+# Some code is taken from:
+# https://www.kaggle.com/ashusma/training-rfcx-tensorflow-tpu-effnet-b2.
+class WarmUpCosine(keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(
+        self, learning_rate_base, total_steps, warmup_learning_rate, warmup_steps
+    ):
+        super(WarmUpCosine, self).__init__()
+
+        self.learning_rate_base = learning_rate_base
+        self.total_steps = total_steps
+        self.warmup_learning_rate = warmup_learning_rate
+        self.warmup_steps = warmup_steps
+        self.pi = tf.constant(np.pi)
+
+    def __call__(self, step):
+        if self.total_steps < self.warmup_steps:
+            raise ValueError("Total_steps must be larger or equal to warmup_steps.")
+
+        cos_annealed_lr = tf.cos(
+            self.pi
+            * (tf.cast(step, tf.float32) - self.warmup_steps)
+            / float(self.total_steps - self.warmup_steps)
+        )
+        learning_rate = 0.5 * self.learning_rate_base * (1 + cos_annealed_lr)
+
+        if self.warmup_steps > 0:
+            if self.learning_rate_base < self.warmup_learning_rate:
+                raise ValueError(
+                    "Learning_rate_base must be larger or equal to "
+                    "warmup_learning_rate."
+                )
+            slope = (
+                self.learning_rate_base - self.warmup_learning_rate
+            ) / self.warmup_steps
+            warmup_rate = slope * tf.cast(step, tf.float32) + self.warmup_learning_rate
+            learning_rate = tf.where(
+                step < self.warmup_steps, warmup_rate, learning_rate
+            )
+        return tf.where(
+            step > self.total_steps, 0.0, learning_rate, name="learning_rate"
+        )
+
+
+def run_experiment(model):
+    total_steps = int((len(trainGen) / BATCH_SIZE) * EPOCHS)
+    warmup_epoch_percentage = 0.10
+    warmup_steps = int(total_steps * warmup_epoch_percentage)
+    scheduled_lrs = WarmUpCosine(
+        learning_rate_base=LEARNING_RATE,
+        total_steps=total_steps,
+        warmup_learning_rate=0.0,
+        warmup_steps=warmup_steps,
+    )
+
+    optimizer = tfa.optimizers.AdamW(
+        learning_rate=LEARNING_RATE, weight_decay=WEIGHT_DECAY
+    )
+
+    model.compile(
+        optimizer=optimizer,
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[
+            keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+            keras.metrics.SparseTopKCategoricalAccuracy(2, name="top-2-accuracy"),
+        ],
+    )
+
+    checkpointer = ModelCheckpoint(
+        monitor="val_accuracy",
+        # monitor="accuracy",
+        filepath="ckpt/ckpt_best.tf",
+        verbose=1,
+        save_best_only=True,
+)
+
+    history = model.fit(
+        x=trainGen,
+        steps_per_epoch=len(list(paths.list_images("train"))) // BATCH_SIZE,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        validation_data=validGen,
+        callbacks=[checkpointer],
+        validation_steps=len(list(paths.list_images("validation"))) // BATCH_SIZE,
+    )
+    # _, accuracy, top_5_accuracy = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE)
+    # print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+    # print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
+    tf.saved_model.save(model, 'saved_model')
+    return history
+
+
+# Run experiments with the vanilla ViT
+# vit = create_vit_classifier(vanilla=True)
+# history = run_experiment(vit)
+
+# Run experiments with the Shifted Patch Tokenization and
+# Locality Self Attention modified ViT
+vit_sl = create_vit_classifier(vanilla=False)
+history = run_experiment(vit_sl)    
+
+"""
+# Final Notes
+
+With the help of Shifted Patch Tokenization and Locality Self Attention,
+we were able to get ~**3-4%** top-1 accuracy gains on CIFAR100.
+
+The ideas on Shifted Patch Tokenization and Locality Self Attention
+are very intuitive and easy to implement. The authors also ablates of
+different shifting strategies for Shifted Patch Tokenization in the
+supplementary of the paper.
+
+I would like to thank [Jarvislabs.ai](https://jarvislabs.ai/) for
+generously helping with GPU credits.
+
+You can use the trained model hosted on [Hugging Face Hub](https://huggingface.co/keras-io/vit_small_ds_v2)
+and try the demo on [Hugging Face Spaces](https://huggingface.co/spaces/keras-io/vit-small-ds).
+"""
